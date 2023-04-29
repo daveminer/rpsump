e23e1ca9355be646e2e11e299d814a7ec761ff54
Dave Miner <wtrispsn@gmail.com>
2023-04-29 18:45:07 -0400

    Fix merge errors (#11)

--- src/controllers/auth/email_verification.rs
+++ src/controllers/auth/email_verification.rs
@@ -6,7 +6,7 @@ use crate::database::DbPool;
 use crate::models::user::User;
 
 #[derive(Debug, Deserialize)]
-pub struct EmailVerificationParamhttps://github.com/daveminer/rpsump/pull/10/conflict?name=src%252Fcontrollers%252Fauth%252Femail_verification.rs&ancestor_oid=402ff2d8aaddb02dc9d7de19f7f79f69f8006da0&base_oid=a6e68a4615e6abc3e5a92557707776a59965d73d&head_oid=ba24b65759b65c89f93ee418569549f1a8c25432s {
+pub struct EmailVerificationParams {
     token: String,
 }
 
