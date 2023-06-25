400af1f987314086afa603ce597f80655423494c
daveminer <wtrispsn@gmail.com>
2023-06-25 14:40:20 -0400

    handle optional sump appdata

--- src/controllers/info.rs
+++ src/controllers/info.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, Responder};
+use actix_web::{get, HttpRequest, Responder};
 use actix_web::{web::Data, Result};
 
 use crate::auth::authenticated_user::AuthenticatedUser;
@@ -6,11 +6,21 @@ use crate::controllers::ApiResponse;
 use crate::sump::Sump;
 
 #[get("/info")]
-async fn info(sump: Option<Data<Sump>>, _user: AuthenticatedUser) -> Result<impl Responder> {
-    let sump = if sump.is_none() {
+async fn info(req: HttpRequest, _user: AuthenticatedUser) -> Result<impl Responder> {
+    let sump_arc = req.app_data::<Data<Option<Sump>>>();
+    let maybe_sump = if sump_arc.is_none() {
         return Ok(ApiResponse::ok("Sump disabled.".to_string()));
     } else {
-        sump.unwrap()
+        // If shared object is present then it should be populated
+        sump_arc.unwrap().as_ref().clone()
+    };
+
+    let sump = match maybe_sump {
+        Some(sump) => sump,
+        None => {
+            tracing::error!("Sump AppData was found but Sump was None");
+            return Ok(ApiResponse::internal_server_error());
+        }
     };
 
     let sensor_state = match sump.sensor_state.lock() {
